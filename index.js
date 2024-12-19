99f170fc4725e55a5753a621a3911a9a76e5b161
Naftaly Ngeru <n.ngeru.k@gmail.com>
2024-12-19 04:20:47 +0300

    Completed Labs

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
 // Code your solution here
+const drivers = ['Bobby', 'Sammy', 'Sally', 'Annette', 'Sarah', 'bobby'];
+function findMatching(array, name){
+    let lowerCaseName = name.toLowerCase();
+    return array.filter(names => names.toLowerCase() === lowerCaseName);
+}
+
+console.log(findMatching(drivers, "Bobby"));
+
+function fuzzyMatch(array, name){
+    let firstTwoLetters = name.slice(0, 2).toLowerCase();
+    return array.filter(names => names.slice(0, 2).toLowerCase() === firstTwoLetters);
+}
+
+console.log(fuzzyMatch(drivers))
+
+function matchName(array, name){
+    return array.filter(names => names.name === name);
+}
+
+console.log(matchName(drivers, "Sally"));
